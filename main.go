5d643e23ce9f3cbd7d9e221880b16501d39c6567
Charles Hsu <charles@6e.com.tw>
2020-02-16 22:41:00 +0800

    增加ImageMessage

--- main.go
+++ main.go
@@ -62,7 +62,10 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if err != nil {
 					log.Println("Quota err:", err)
 				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.PreviewImageURL+message.OriginalContentURL+" OK! ImageMessage, remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+				if _, err = bot.ReplyMessage(
+					event.ReplyToken,
+					linebot.NewStickerMessage("1", "2"),
+					linebot.NewTextMessage(message.ID+":"+message.PreviewImageURL+message.OriginalContentURL+" OK! ImageMessage, remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
 					log.Print(err)
 				}
 			}
